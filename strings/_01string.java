f51b7b6ee8b2dcd5165b1a1ed2b69ea1345e40b3
Adarsh Singh <adarsh22082012@gmail.com>
2025-09-17 23:27:54 +0530

    string-java

---
+++ strings/_01string.java
@@ -0,0 +1,13 @@
+package strings;
+
+public class _01string {
+  public static void main(String[] args) {
+    int a = 10;
+    int [] arr = {2,4,19,5};
+    //collection of character
+    String name = "Messi";
+    System.out.println(name);
+
+  }
+  
+}
